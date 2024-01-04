bc4e12cbbfaf47a9805ec903342673c915b79b9c
viakondratiuk <viakondratiuk@gmail.com>
2024-01-04 13:57:25 +0200

    Merge default and CATEGORY routes

--- src/routes.ts
+++ src/routes.ts
@@ -8,10 +8,27 @@ import {
     getTotalReviews,
     getChart, getLocation,
 } from './profile.js';
+import { labels } from './consts.js';
 
 export const router = createCheerioRouter();
 
-router.addHandler('PROFILE', async ({ $, request, log }) => {
+router.addDefaultHandler(async ({ $, enqueueLinks, request, log }) => {
+    await enqueueLinks({
+        selector: 'ul.directory-list h3.company_info > a',
+        label: labels.PROFILE,
+    });
+
+    // Find the "Next" button and enqueue the next page of results (if it exists)
+    const nextButton = $('li.page-item.next a.page-link');
+    if (nextButton) {
+        log.debug(`Enqueueing pagination for: ${request.url}`);
+        await enqueueLinks({
+            selector: 'li.page-item.next a.page-link',
+        });
+    }
+});
+
+router.addHandler(labels.PROFILE, async ({ $, request, log }) => {
     log.debug(`Extracting data: ${request.url}`);
 
     const chart = getChart($);
@@ -32,37 +49,4 @@ router.addHandler('PROFILE', async ({ $, request, log }) => {
 
     log.debug(`Saving data: ${request.url}`);
     await Dataset.pushData(results);
-});
-
-router.addHandler('CATEGORY', async ({ $, enqueueLinks, request, log }) => {
-    log.debug(`Enqueueing pagination for: ${request.url}`);
-
-    await enqueueLinks({
-        selector: 'ul.directory-list h3.company_info > a',
-        label: 'PROFILE',
-    });
-
-    // Find the "Next" button and enqueue the next page of results (if it exists)
-    const nextButton = $('li.page-item.next a.page-link');
-    if (nextButton) {
-        await enqueueLinks({
-            selector: 'li.page-item.next a.page-link',
-            label: 'CATEGORY',
-        });
-    }
-});
-
-// This is a fallback route which will handle the start URL
-// as well as the LIST labeled URLs.
-router.addDefaultHandler(async ({ $, enqueueLinks, request, log }) => {
-    log.debug(`Enqueueing START page categories: ${request.url}`);
-
-    // Find the "Next" button and enqueue the next page of results (if it exists)
-    const nextButton = $('li.page-item.next a.page-link');
-    if (nextButton) {
-        await enqueueLinks({
-            selector: 'li.page-item.next a.page-link',
-            label: 'CATEGORY',
-        });
-    }
 });
